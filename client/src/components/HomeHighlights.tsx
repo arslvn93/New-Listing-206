4d4700dcac0ce7b2a156e81c3cd532c3f07fcb1d
arslvn93 <support@forms.ltd>
2025-11-20 15:40:16 -0500

    Big Update to New Site

--- client/src/components/HomeHighlights.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect, useRef } from 'react';
-import { homeShowcaseSections } from '../config/siteConfig';
-
-const HomeHighlights = () => {
-  // Temporarily hide the BEAUTIFUL SPACES section
-  return null;
-};
-
-export default HomeHighlights;
